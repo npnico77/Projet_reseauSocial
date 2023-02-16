8ebebcdd34261df5174330c929f5739427fdfcea
nicolas perreau <npnicolas@sfr.fr>
2023-02-17 00:12:40 +0100

    ok final back

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const userRoutes = require('./routes/user.routes');
-const postRoutes = require('./routes/post.routes');
-require('dotenv').config({path: './config/.env'});
-require('./config/db');
-const {checkUser, requireAuth} = require('./middleware/auth.middleware.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware.js");
+const cors = require("cors");
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 //jwt
-app.get('*', checkUser);
-app.get('/jwtid', requireAuth, (req, res) => {
-    res.status(200).send(res.locals.user._id);
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
 });
 
 // routes
-app.use('/api/user', userRoutes);
-app.use('/api/post', postRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 // server
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`)
+  console.log(`Listening on port ${process.env.PORT}`);
 });
